23e83244627e29a0be98d28810fc939176841a6d
Trevor <googlrr>
2017-04-28 13:42:19 -0400

    Started documentation of h5 metadata

--- src/h5/metadata.js
+++ src/h5/metadata.js
@@ -1,87 +1,190 @@
 var httpWrapper = require("../httpWrapper.js");
 
-function metadata(API_KEY){
+/** Creates a metadata object
+* @exports h5/metadata
+* @constructor
+*/
+module.exports = function metadata(API_KEY){
+
+  /** Initializes a new httpWrapper with API Key */
   this._http = new httpWrapper(API_KEY);
-}
 
-metadata.prototype.campaignMissions = function(){
-  return this._http.queryAPI('/metadata/h5/metadata/campaign-missions');
-}
+  /**
+  * Returns Campaign Missions </br>
+  * {@link https://developer.haloapi.com/docs/services/58ace18c21091812784ce8c5/operations/58ace18c2109180bdcacc421}
+  * @example
+  * halo.h5.metadata.campaignMissions().then( (missions) => {
+  * //Do code here
+  * });
+  * @returns {Promise} Promise of JSON from API
+  */
+  metadata.prototype.campaignMissions = function(){
+    return this._http.queryAPI('/metadata/h5/metadata/campaign-missions');
+  }
 
-metadata.prototype.commendations = function(){
-  return this._http.queryAPI('/metadata/h5/metadata/commendations');
-}
+  /**
+  * Returns Commendations </br>
+  * {@link https://developer.haloapi.com/docs/services/58ace18c21091812784ce8c5/operations/58ace18c2109180bdcacc422}
+  * @example
+  * halo.h5.metadata.commendations().then( (commendations) => {
+  * //Do code here
+  * });
+  * @returns {Promise} Promise of JSON from API
+  */
+  metadata.prototype.commendations = function(){
+    return this._http.queryAPI('/metadata/h5/metadata/commendations');
+  }
 
-metadata.prototype.csrDesignations = function(){
-  return this._http.queryAPI('/metadata/h5/metadata/csr-designations');
-}
+  /**
+  * Returns CSR Designations </br>
+  * {@link https://developer.haloapi.com/docs/services/58ace18c21091812784ce8c5/operations/58ace18c2109180bdcacc423}
+  * @example
+  * halo.h5.metadata.csrDesignations().then( (designations) => {
+  * //Do code here
+  * });
+  * @returns {Promise} Promise of JSON from API
+  */
+  metadata.prototype.csrDesignations = function(){
+    return this._http.queryAPI('/metadata/h5/metadata/csr-designations');
+  }
 
-metadata.prototype.enemies = function(){
-  return this._http.queryAPI('/metadata/h5/metadata/enemies');
-}
+  /**
+  * Returns Enemies </br>
+  * {@link https://developer.haloapi.com/docs/services/58ace18c21091812784ce8c5/operations/58ace18c2109180bdcacc424}
+  * @example
+  * halo.h5.metadata.enemies().then( (enemies) => {
+  * //Do code here
+  * });
+  * @returns {Promise} Promise of JSON from API
+  */
+  metadata.prototype.enemies = function(){
+    return this._http.queryAPI('/metadata/h5/metadata/enemies');
+  }
 
-metadata.prototype.flexibleStats = function(){
-  return this._http.queryAPI('/metadata/h5/metadata/flexible-stats');
-}
+  /**
+  * Returns Flexible Stats</br>
+  * {@link https://developer.haloapi.com/docs/services/58ace18c21091812784ce8c5/operations/58ace18c2109180bdcacc425}
+  * @example
+  * halo.h5.metadata.flexibleStats().then( (stats) => {
+  * //Do code here
+  * });
+  * @returns {Promise} Promise of JSON from API
+  */
+  metadata.prototype.flexibleStats = function(){
+    return this._http.queryAPI('/metadata/h5/metadata/flexible-stats');
+  }
 
-metadata.prototype.gameBaseVariants = function(){
-  return this._http.queryAPI('/metadata/h5/metadata/game-base-variants');
-}
+  /**
+  * Returns Game Base Variants</br>
+  * {@link https://developer.haloapi.com/docs/services/58ace18c21091812784ce8c5/operations/58ace18c2109180bdcacc426}
+  * @example
+  * halo.h5.metadata.gameBaseVariants().then( (variants) => {
+  * //Do code here
+  * });
+  * @returns {Promise} Promise of JSON from API
+  */
+  metadata.prototype.gameBaseVariants = function(){
+    return this._http.queryAPI('/metadata/h5/metadata/game-base-variants');
+  }
 
-metadata.prototype.gameVariants = function(id){
-  return this._http.queryAPI('/metadata/h5/metadata/game-variants/' + id);
-}
+  /**
+  * Returns Game Variants</br>
+  * {@link https://developer.haloapi.com/docs/services/58ace18c21091812784ce8c5/operations/58ace18c2109180bdcacc427}
+  * @example
+  * halo.h5.metadata.gameVariants().then( (variants) => {
+  * //Do code here
+  * });
+  * @returns {Promise} Promise of JSON from API
+  */
+  metadata.prototype.gameVariants = function(id){
+    return this._http.queryAPI('/metadata/h5/metadata/game-variants/' + id);
+  }
 
-metadata.prototype.impulses = function(){
-  return this._http.queryAPI('/metadata/h5/metadata/impulses');
-}
+  /**
+  * Returns Impulses</br>
+  * {@link https://developer.haloapi.com/docs/services/58ace18c21091812784ce8c5/operations/58ace18c2109180bdcacc428}
+  * @example
+  * halo.h5.metadata.impulses().then( (impulses) => {
+  * //Do code here
+  * });
+  * @returns {Promise} Promise of JSON from API
+  */
+  metadata.prototype.impulses = function(){
+    return this._http.queryAPI('/metadata/h5/metadata/impulses');
+  }
 
-metadata.prototype.mapVariant = function(id){
-  return this._http.queryAPI('/metadata/h5/metadata/map-variants/' + id);
-}
+  /**
+  * Returns Map Variant</br>
+  * {@link https://developer.haloapi.com/docs/services/58ace18c21091812784ce8c5/operations/58ace18c2109180bdcacc429}
+  * @example
+  * halo.h5.metadata.mapVariant().then( (variant) => {
+  * //Do code here
+  * });
+  * @returns {Promise} Promise of JSON from API
+  */
+  metadata.prototype.mapVariant = function(id){
+    return this._http.queryAPI('/metadata/h5/metadata/map-variants/' + id);
+  }
 
-metadata.prototype.maps = function(){
-  return this._http.queryAPI('/metadata/h5/metadata/maps');
-}
+  /**
+  * Returns Maps</br>
+  * {@link https://developer.haloapi.com/docs/services/58ace18c21091812784ce8c5/operations/58ace18c2109180bdcacc42a}
+  * @example
+  * halo.h5.metadata.maps().then( (maps) => {
+  * //Do code here
+  * });
+  * @returns {Promise} Promise of JSON from API
+  */
+  metadata.prototype.maps = function(){
+    return this._http.queryAPI('/metadata/h5/metadata/maps');
+  }
 
-metadata.prototype.medals = function(){
-  return this._http.queryAPI('/metadata/h5/metadata/medals');
-}
+  /**
+  * Returns medals</br>
+  * {@link https://developer.haloapi.com/docs/services/58ace18c21091812784ce8c5/operations/58ace18c2109180bdcacc42b}
+  * @example
+  * halo.h5.metadata.medals().then( (maps) => {
+  * //Do code here
+  * });
+  * @returns {Promise} Promise of JSON from API
+  */
+  metadata.prototype.medals = function(){
+    return this._http.queryAPI('/metadata/h5/metadata/medals');
+  }
 
-metadata.prototype.playlists = function(){
-  return this._http.queryAPI('/metadata/h5/metadata/playlists');
-}
+  metadata.prototype.playlists = function(){
+    return this._http.queryAPI('/metadata/h5/metadata/playlists');
+  }
 
-metadata.prototype.requisition = function(id){
-  return this._http.queryAPI('/metadata/h5/metadata/requisitions/' + id);
-}
+  metadata.prototype.requisition = function(id){
+    return this._http.queryAPI('/metadata/h5/metadata/requisitions/' + id);
+  }
 
-metadata.prototype.requisitionPack = function(id){
-  return this._http.queryAPI('/metadata/h5/metadata/requisition-packs/' + id);
-}
+  metadata.prototype.requisitionPack = function(id){
+    return this._http.queryAPI('/metadata/h5/metadata/requisition-packs/' + id);
+  }
 
-metadata.prototype.seasons = function(){
-  return this._http.queryAPI('/metadata/h5/metadata/seasons');
-}
+  metadata.prototype.seasons = function(){
+    return this._http.queryAPI('/metadata/h5/metadata/seasons');
+  }
 
-metadata.prototype.skulls = function(){
-  return this._http.queryAPI('/metadata/h5/metadata/skulls');
-}
+  metadata.prototype.skulls = function(){
+    return this._http.queryAPI('/metadata/h5/metadata/skulls');
+  }
 
-metadata.prototype.spartanRanks = function(){
-  return this._http.queryAPI('/metadata/h5/metadata/spartan-ranks');
-}
+  metadata.prototype.spartanRanks = function(){
+    return this._http.queryAPI('/metadata/h5/metadata/spartan-ranks');
+  }
 
-metadata.prototype.teamColors = function(){
-  return this._http.queryAPI('/metadata/h5/metadata/team-colors');
-}
+  metadata.prototype.teamColors = function(){
+    return this._http.queryAPI('/metadata/h5/metadata/team-colors');
+  }
 
-metadata.prototype.vehicles = function(){
-  return this._http.queryAPI('/metadata/h5/metadata/vehicles');
-}
+  metadata.prototype.vehicles = function(){
+    return this._http.queryAPI('/metadata/h5/metadata/vehicles');
+  }
 
-metadata.prototype.weapons = function(){
-  return this._http.queryAPI('/metadata/h5/metadata/weapons');
+  metadata.prototype.weapons = function(){
+    return this._http.queryAPI('/metadata/h5/metadata/weapons');
+  }
 }
-
-module.exports = metadata;
