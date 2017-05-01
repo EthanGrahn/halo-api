99001428fdd6f75ab96ac8ace02db3e501093af4
Trevor Carmichael <Googlrr@gmail.com>
2017-04-30 21:57:07 -0400

    did some api testing to make sure things weren't working. All of the commented examples appear to function correctly

--- Index.js
+++ Index.js
@@ -20,16 +20,40 @@ var halo = new haloWrapper(process.env.API_KEY);
 // halo.hw2.stats.playerCampaignProgress('googlrr').then((stats)=>{
 //   console.log(stats);
 // });
+//
 
+// For getting the playlist IDs (needed for other APIs)
 // halo.hw2.metadata.playlists().then((playlists) => {
-//   console.log(playlists.ContentItems);
+//   for(var x = 0; x < playlists.ContentItems.length; x++) {
+//       console.log(playlists.ContentItems[x].View.Title + "   Identity: " + playlists.ContentItems[x].View.Identity);
+//   }
 // });
 
-halo.hw2.stats.playerPlaylistRatings('a177b19a32164962abdc4653dfb7911b', 'googlrr').then((ratings)=>{
-  console.log(ratings);
-});
+// halo.hw2.stats.playerPlaylistRatings('a177b19a32164962abdc4653dfb7911b', 'googlrr').then((ratings)=>{
+//   console.log(ratings);
+// });
 // halo.hw2.stats.playerXPs('googlrr').then( (ratings) =>{
 //   console.log(ratings);
 // }).catch( (rejected) => {
 //   console.log("oh no");
 // });
+
+// halo.hw2.stats.playerPlaylistRatings('532bfd6c3db445b7a01011460b862be6', 'Googlrr').then((ratings)=>{
+//   console.log(ratings.Results);
+//   for(let x = 0; x < ratings.Results.length; x++){
+//     console.log(ratings.Results[x]);
+//   }
+// });
+
+// halo.hw2.metadata.gameObjects().then( (objects) => {
+//   //console.log(objects);
+//   let content = objects.ContentItems;
+//   for(let x = 0; x < content.length; x++){
+//     console.log(content[x].View.HW2Object.DisplayInfo.View.HW2ObjectDisplayInfo.Name);//.HW2ObjectDisplayInfo + "\t" + content[x].View.Identity);
+//   }
+// });
+
+
+halo.hw2.metadata.techs().then( (techs) => {
+  console.log(techs.ContentItems);
+});
